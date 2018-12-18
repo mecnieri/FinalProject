dd814a9c0641efca9d4f31339503a0862ce15c7f
mecnieri <abzogio@yahoo.com>
2018-12-18 21:23:04 +0400

    56565

--- Front/src/components/Product.js
+++ Front/src/components/Product.js
@@ -2,7 +2,8 @@ import React from 'react'
 function Product(props){
     return(
         <div><h1>{props.product.productName}</h1>
-        <img src={`${props.product.image}`} />
+        <img src={props.product.image} alt={props.product.productName} width="300px" height="300px" />
+        {/* <img src={`${props.product.image}`} /> */}
         </div>
     )
 }
