e6f518cb689b1e303e577e0252ea0a7022d26690
Nikoloz Asatiani <asatiani.nikoloz@gmail.com>
2018-12-18 11:18:52 +0400

    bla bla

--- Saboloo/src/components/SignupPage.js
+++ Saboloo/src/components/SignupPage.js
@@ -7,11 +7,8 @@ export default class SignupPage extends React.Component {
     constructor(props){
         super(props)
         this.username = React.createRef();
-        this.mail = React.createRef();
+        this.mail=React.createRef();
         this.password = React.createRef();
-        this.password2 = React.createRef();
-        this.balance = React.createRef();
-        this.birthday = React.createRef();        
     }
 
 
@@ -28,31 +25,36 @@ export default class SignupPage extends React.Component {
         <div className="input-icon"><i className="fa fa-user"></i></div>
       </div>
       <div className="input-group input-group-icon">
-        <input type="email" placeholder="E-mail" ref="email" required/>
+        <input type="email" placeholder="E-mail" ref="e-mail" required/>
         <div className="input-icon"><i className="fa fa-envelope"></i></div>
       </div>
       <div className="input-group input-group-icon">
         <input type="password" ref="password" placeholder="Password" required/>
-        <input type="password" ref="password2" placeholder="Confirm Password" required/>
         <div className="input-icon"><i className="fa fa-key"></i></div>
       </div>
     </div>
     <div className="row">
       <div className="col-half">
-        <h4>birthday</h4>
+        <h4>Date of Birth</h4>
         <div className="input-group date">
-          <div>
-            <input type="date" ref="birthday" placeholder="DD" required/>
+          <div className="col-third">
+            <input type="number" ref="day" placeholder="DD" required/>
+          </div>
+          <div className="col-third">
+            <input type="number" ref="month" placeholder="MM" required/>
+          </div>
+          <div className="col-third">
+            <input type="number" ref="year" placeholder="YYYY" required/>
           </div>
         </div>
       </div>
       
     </div>
     <div className="row">
-      <h4>Balance</h4>
+      <h4>Payment Details</h4>
       
       <div className="input-group input-group-icon">
-        <input ref="balance" type="text" placeholder="Balance"/>
+        <input type="text" placeholder="Card Number"/>
         <div className="input-icon"><i className="fas fa-address-card"></i></div>
       </div>
     </div>
