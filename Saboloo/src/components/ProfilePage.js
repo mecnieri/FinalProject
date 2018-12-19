4b4b9b581ae71e5b39acfca1744ef6f7a201e2a8
VakhoNodadze <vaxonodadze@gmail.com>
2018-12-20 03:06:24 +0400

    Dizainit 2

--- Saboloo/src/components/ProfilePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ProfilePage = props => {
-    return (
-        <div className="profile">
-            <h1 className="profile--title">Welcome, { localStorage.getItem('authorized') }</h1>
-        </div>
-    )
-}
