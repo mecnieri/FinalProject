55c6650156b4b3268d8160a53830a212f87beed9
mecnieri <abzogio@yahoo.com>
2018-12-19 20:56:28 +0400

    slider and everything

--- Front/src/components/Slider.jsx
+++ Front/src/components/Slider.jsx
@@ -1,108 +1,37 @@
 import React, { Component } from 'react';
-import Arrows from './Arrows';
+import { Slide } from 'react-slideshow-image';
+import img1 from '../images/prod1.png';
+import img2 from '../images/prod2.png';
+import img3 from '../images/prod3.jpg';
 
 
 
-class Slider extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      images: [
-        './images/prod1.png',
-        './images/prod2.png',
-        './images/prod3.jpg',
-        'https://placekitten.com/500/503',
-        
-      ],
-      currentSlide: 0,
-      translateValue: 0,
-      // automaticMove:true
-    }
-
-  }
-  
-  prevSlide = () => {
-    if( this.state.currentSlide === 0 ) return;
-    const containerWidth = document.querySelector('.slider').clientWidth;
-    this.setState( prevState => {
-      return {
-        currentSlide: prevState.currentSlide - 1,
-        translateValue: prevState.translateValue  + containerWidth,
-      }
-    })
-  }
-  nextSlide = () => {
-    if( (this.state.images.length - 1 )  === this.state.currentSlide ){
-      return;
-    }
-    const containerWidth= document.querySelector('.slider').clientWidth;
-    this.setState(  (nextState ) => {
-      return {
-        currentSlide: nextState.currentSlide + 1,
-        translateValue: nextState.translateValue-containerWidth,
-      }
-    })
-
-  }
-  // automaticMovement=()=>{
-  //   const containerWidth= document.querySelector('.slider').clientWidth;
-  //   this.setState({
-  //     translateValue: this.state.translateValue-containerWidth,
-  //   })
-  //   return (
-  //     <div className="slider">
-  //     <div className="slider_container"
-  //         style={{
-  //           transform: `translateX(${this.state.translateValue}px)`,
-  //           transition: 'transform linear 0.5s',
-  //           width: `${100}%`
-  //         }}>
-  //         {
-  //           this.createImages()
-  //         }
-  //     </div>
-  //     </div>
-  //   )
-  // }
-
-  createImages=()=>{
-    let imageArray=[];
-    for(let i=0;i<this.state.images.length;i++){
-      imageArray.push(<div style={{width: `${100}%`,display:'inline-block'}} key={i}><img src={this.state.images[i]} 
-      style={{width: `${50}%`, display:'flex', margin:'auto'}}key={i} alt="slider_image"/></div>)
-    }
-    return imageArray;
-  }
-  
-  render() {
-    // if(this.state.automaticMove){
-    //   //setInterval(function(){
-    //     this.automaticMovement()
-   
-      
-    // }
-    return (
-      <div className="slider">
-          <div className="slider_container"
-          style={{
-            transform: `translateX(${this.state.translateValue}px)`,
-            transition: 'transform linear 0.5s',
-            width: `${100}%`
-          }}>
-          {
-            this.createImages()
-          }
-          </div>
-        <Arrows.LeftArrow prevSlide={this.prevSlide} />
-        <Arrows.RightArrow nextSlide={this.nextSlide}/>
-
-      </div>
-    );
-  }
+const properties = {
+  duration: 3000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  autoplay: true
 }
 
+const Slideshow = () => {
+  return (
+    <Slide {...properties}>
+      <div className="each-slide">
+        <img src={img1} alt="img1" width="600" height="400" />
+        <p>New Mobiles</p>
+      </div>
+      <div className="each-slide">
+        <img src={img2} alt="img2" width="500" height="400" />
+        <span>Slide 2</span>
+      </div>
+      <div className="each-slide">
+        <img src={img3} alt="img3" width="500" height="400" />
+        <span>Slide 3</span>
+      </div>
+    </Slide>
+  )
+}
 
-
-
-export default Slider;
+export default Slideshow;
