251df8d64e314cff14bbcad7ec91b1a534683fe7
Nikoloz Asatiani <asatiani.nikoloz@gmail.com>
2018-12-19 20:44:56 +0400

    bla bla bla

---
+++ Front/src/items.js
@@ -0,0 +1,5 @@
+export default [
+    { id: 1, name: 'Iphone X', brand: 'Apple', quantity: 1, price: 1000, shipping: 50, limit: 12 },
+    { id: 2, name: 'Laptop X556', brand: 'Asus', quantity: 1, price: 1500, shipping: 0, limit: 10 },
+    { id: 3, name: 'S9', brand: 'Samsung', quantity: 1, price: 800, shipping: 0, limit: 8 },
+];
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//Create Schema
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: false
+  },
+  birthday: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: String,
+    required: true
+  },
+  inbox: {
+    type: Array
+  },
+  cart: {
+    type: Array
+  },
+  boughtProducts: {
+    type: Array
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema)
