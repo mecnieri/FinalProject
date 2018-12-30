f3cee93152770e123b238948bc3458167ee8be81
mecnieri <abzogio@yahoo.com>
2018-12-30 04:25:16 +0400

    Front

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('Front/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'Front', 'build', 'index.html'))
     })
 }
 
